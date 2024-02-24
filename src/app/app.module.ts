6a09a0ae464b0801394420949dc909dd5f5c41ad
arunavanew52486 <arunavanew52486@gmail.com>
2024-02-24 14:14:20 +0530

    routing added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ErrorComponent } from './error/error.component';
+import { PaymentComponent } from './payment/payment.component';
+
+const routes:Routes = [
+    { path: "", component: HomeComponent },
+    { path: "payment", component: PaymentComponent },
+    { path: "**", component: ErrorComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    PaymentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
